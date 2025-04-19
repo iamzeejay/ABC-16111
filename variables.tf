449b38da26ddafe448ef375da2a08d89ffae5add
iamzeejay <zeeshan2308online@gmail.com>
2025-04-19 08:12:15 +0530

    Initial commit with vnets, peering, and VMs

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "location" {
+  default = "East US"
+}
+
+variable "resource_group_name" {
+  default = "rg-terraform-demo"
+}
+
+variable "username" {
+  default = "azureuser"
+}
+
+variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
+}
+
